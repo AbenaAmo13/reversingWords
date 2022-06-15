692b90f66580fbd516ebf9476bcbc8e87e31a73e
AbenaAmo13 <60615703+AbenaAmo13@users.noreply.github.com>
2022-06-15 22:05:40 +0100

    feat/WIP : Successfully reversed words with letter that I have a length of over 4. T

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,44 +3,38 @@ package com.company;
 import java.util.*;
 
 public class Main {
-    public String breakDownWordReverse(String word){
+    public String breakDownWordReverse(String word) {
+        StringBuilder sortedWord = new StringBuilder();
         String reverseWord = null;
         List sortWord = new ArrayList();
         String[] splitWord = word.split("");
         sortWord = Arrays.asList(splitWord);
         Collections.reverse(sortWord);
-        System.out.println(sortWord);
-
-
-return reverseWord;
-
+        for (Object letter : sortWord) {
+            sortedWord.append(letter);
+        }
+        System.out.println(sortedWord.toString());
+        return sortedWord.toString();
     }
+
     public String spinWords(String sentence) {
         //TODO: Code stuff here
         StringBuilder finalSentence = new StringBuilder();
-        String finalWord =null;
+        String finalWord = null;
         String[] words = sentence.split(" ");
-        for(String word: words ){
-            if(word.length()>4){
-                finalWord = breakDownWordReverse(word);
-                System.out.println(word);
-
-                finalWord = word;
-            }else{
+        for (String word : words) {
+            if (word.length() > 4) {
+                finalSentence.append(breakDownWordReverse(word) + " ");
+            } else {
                 finalSentence.append(word + " ");
-                finalWord = finalSentence.toString();
-
             }
-
         }
-
-
+        finalWord = finalSentence.toString();
         return finalWord;
-
     }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
         Main testMain = new Main();
